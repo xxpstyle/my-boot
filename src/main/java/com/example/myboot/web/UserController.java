017d523b6cca0f5b8a5f7099212493cbb326ed2c
xxp <xxpstyle@163.com>
2019-10-23 17:16:58 +0800

    集成swagger2

--- src/main/java/com/example/myboot/web/UserController.java
+++ src/main/java/com/example/myboot/web/UserController.java
@@ -1,6 +1,8 @@
 package com.example.myboot.web;
 
 import com.example.myboot.dao.UserDao;
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,6 +12,8 @@ public class UserController {
     @Autowired
    private UserDao userDao;
 
+    @ApiOperation(value = "接口的功能介绍",notes = "提示接口使用者注意事项",httpMethod = "GET")
+    @ApiImplicitParam(dataType = "string",name = "name",value = "姓名",required = true)
     @RequestMapping("/abc")
     public String hello(String name) {
         System.out.println(name);
